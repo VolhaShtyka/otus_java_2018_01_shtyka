03975e13ccf0bcfdc6ae28bb61a1442b2ab721bf
Volha <3470097@gmail.com>
2018-02-10 23:31:52 +0300

    memory agent

---
+++ L02memory/src/main/java/ru/otus/l02/AgentMemory.java
@@ -0,0 +1,20 @@
+package ru.otus.l02;
+
+import java.lang.instrument.Instrumentation;
+
+public class AgentMemory {
+    private static volatile Instrumentation instrumentation;
+
+    public static void premain(String args, Instrumentation inst) {
+        instrumentation = inst;
+        System.out.println("Hello! I`m java agent");
+
+    }
+
+    public static long getSize(Object obj) {
+        if (instrumentation == null) {
+            throw new IllegalStateException("Agent not initialised");
+        }
+        return instrumentation.getObjectSize(obj);
+    }
+}
---
+++ L02memory/src/main/java/ru/otus/l02/Main.java
@@ -0,0 +1,52 @@
+package ru.otus.l02;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        printObjectSize(new Object());
+        printObjectSize(new AgentTester());
+        printObjectSize(new AgentTester(2, "I`m test", 873635L));
+        printObjectSize(1);
+        printObjectSize("");
+        printObjectSize("string");
+        printObjectSize(new BigDecimal("999999999999999.999"));
+        printObjectSize(new ArrayList<String>());
+        printObjectSize(new Integer[100]);
+    }
+
+    private static void printObjectSize(Object obj) {
+        System.out.println(String.format("%s: %s, size=%s", obj.getClass()
+                .getSimpleName(), obj, AgentMemory.getSize(obj)));
+    }
+}
+
+class AgentTester {
+    private int number;//4 bytes
+    private String name; //  12 bytes (char value[]; 4 bytes int offset; // 4 bytes int count; // 4 bytes)
+    private Long id; //8 bytes
+
+    AgentTester() {
+    }
+
+    AgentTester(int number, String name, Long id) {
+        super();
+        this.number = number;
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        if (name == null) {
+            return "I`m empty";
+        } else {
+            return "AgentTester{" +
+                    "number=" + number +
+                    ", name='" + name + '\'' +
+                    ", id=" + id +
+                    '}';
+        }
+    }
+}
